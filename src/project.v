c209e0f111ac3a3aa5564fef1790f2315927154c
emma <eoommaaa@gmail.com>
2026-04-19 19:22:36 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -27,7 +27,7 @@ module tt_um_eoommaa_prbs31 (
 end
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = lfsr[30];
+  assign uo_out[0]  = lfsr[30];
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uo_out[7:1] = 7'd0;
